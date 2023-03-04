e3e312ccf5d302c435bbeb077f635edfefb56558
yashnarkhede <yash.r.narkhede@gmail.com>
2023-03-04 17:15:41 +0530

    added poll page

---
+++ dapp/src/pages/CreatePoll.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default function Register() {
+    return (
+        <div class="m-auto xl:container px-12 sm:px-0 mx-auto">
+            <div class="mx-auto h-full sm:w-max">
+                <div class="m-auto  py-12">
+                    <div class="mt-12 rounded-3xl border bg-gray-50 dark:border-gray-700 dark:bg-gray-800 -mx-6 sm:-mx-10 p-8 sm:p-10">
+                        <h3 class="text-2xl font-semibold text-gray-700 dark:text-white">
+                            Poll Register
+                        </h3>
+
+                        <form action="" class="mt-10 space-y-8 dark:text-white" />
+                        <div>
+                            <div class="relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400 dark:before:bg-sky-800 focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300">
+                                <input
+                                    id=""
+                                    type="email"
+                                    placeholder="State"
+                                    class="w-full bg-transparent pb-3  border-b border-gray-300 dark:placeholder-gray-300 dark:border-gray-600 outline-none  invalid:border-red-400 transition"
+                                />
+                            </div>
+
+                            <div>
+                                <br />
+                            </div>
+
+                            <div class="flex flex-col items-end">
+                                <div class="w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400 dark:before:bg-sky-800 focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300">
+                                    <input
+                                        id=""
+                                        type="Your password"
+                                        placeholder="Position"
+                                        class="w-full bg-transparent pb-3  border-b border-gray-300 dark:placeholder-gray-300 dark:border-gray-600 outline-none  invalid:border-red-400 transition"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <br />
+                            </div>
+
+
+                            <div class="relative max-w-sm">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                                </div>
+                                <input datepicker type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
+                            </div>
+
+
+
+                            <div>
+                                <br />
+                            </div>
+                            <form>
+                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="default_size">Upload Imagee</label>
+                                <input class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="default_size" type="file" />
+
+
+
+                                <div>
+                                    <button class="w-full rounded-full bg-sky-500 dark:bg-sky-400 h-11 flex items-center justify-center px-6 py-3 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800">
+                                        <span class="text-base font-semibold text-white dark:text-gray-900">
+                                            Register
+                                        </span>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
+}
